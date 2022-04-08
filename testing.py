9ada9c98c90d5b9753e16e02d3024a412d47f505
k-diep <62728867+k-diep@users.noreply.github.com>
2022-04-08 17:01:57 -0500

    Add files via upload
    Fixed to proper format

--- testing.py
+++ testing.py
@@ -16,17 +16,19 @@ client = bigquery.Client(credentials=credentials, project=credentials.project_id
 
 #CHANGE THIS
 table_ref = 'flask-upload-345423.sample.email'
-table_sch = ''
+table_sch = []
 try:
     SchemaJob = client.get_table(table_ref)
     print("Table Schema: {}".format(SchemaJob.schema))
     for s in SchemaJob.schema:
-        table_sch += s.name + ":" + s.field_type + "," 
+        new_dict = {}
+        new_dict['name'] = s.name
+        new_dict['type'] = s.field_type
+        new_dict['mode'] = s.mode
+        table_sch.append(new_dict)
     
-    #Gets rid of last character ","
-    table_sch = table_sch[:-1]
     print(table_sch)
-    #OUTPUT: 'name:STRING,email:STRING,id:FLOAT'
+    #OUTPUT: [{'name': 'name', 'type': 'STRING', 'mode': 'NULLABLE'}, {'name': 'email', 'type': 'STRING', 'mode': 'NULLABLE'}, {'name': 'id', 'type': 'FLOAT', 'mode': 'NULLABLE'}]
 except Exception as e:
     print(e)
         
